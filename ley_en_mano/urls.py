d851743f1929fc32427cdb3cd6428149b650369a
VictorAponteParedes <vaponte520@gmail.com>
2023-11-28 21:49:59 -0300

    Se crean las url o rutas para accederlos

--- ley_en_mano/urls.py
+++ ley_en_mano/urls.py
@@ -1,11 +1,21 @@
 from rest_framework import routers
 from django.urls import include, path
-from .views import CustomUserViewset
+from .views import (
+    CustomUserViewset,
+    ClienteViewset,
+    AbogadoViewset,
+    EspecialidadViewset,
+    CasoViewset,
+)
 
 
 routers = routers.DefaultRouter()
 
 routers.register(r"usuarios", CustomUserViewset, "usuarios")
+routers.register(r"cliente", ClienteViewset, "cliente")
+routers.register(r"abogado", AbogadoViewset, "abogado")
+routers.register(r"especialidad", EspecialidadViewset, "especialidad")
+routers.register(r"caso", CasoViewset, "caso")
 
 
 urlpatterns = [path("", include(routers.urls))]
