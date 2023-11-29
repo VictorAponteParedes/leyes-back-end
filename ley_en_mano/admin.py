5a3a2e58ba1a22113805e0c3a487ce46560fac6b
VictorAponteParedes <vaponte520@gmail.com>
2023-11-28 21:49:27 -0300

    Agrega: en el admin los modelos creados

--- ley_en_mano/admin.py
+++ ley_en_mano/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Caso, Cliente, Especialidad, Abogado
 
 # Register your models here.
 
 
 admin.site.register(CustomUser)
+admin.site.register(Caso)
+admin.site.register(Cliente)
+admin.site.register(Especialidad)
+admin.site.register(Abogado)
